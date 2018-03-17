b182fa7b994338cabe9716611a9b0ca8cc7453ff
Yfling <598184085@qq.com>
2018-03-17 22:55:20 +0800

    角色组件新增翻页功能

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,7 +9,7 @@
 import global_ from './components/Global'//引用文件
 import VueRouter from 'vue-router';
 import routes from './routes';    // 路由配置文件
-import Vuex from 'vuex'
+import Vuex from 'vuex';
 
 require('./bootstrap');
 window.Vue = require('vue');
@@ -62,7 +62,6 @@ let store = new Vuex.Store({
       state.userData = userData;
     },
     setPermissionIdList: function (state, permissionIdList) {
-      console.log('--')
       state.permissionIdList = permissionIdList;
     }
   },
