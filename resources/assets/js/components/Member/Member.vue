430905ed2370298da4a10460f6eef398f2796715
Yfling <598184085@qq.com>
2018-03-21 20:16:39 +0800

    member component improvement

--- resources/assets/js/components/Member/Member.vue
+++ resources/assets/js/components/Member/Member.vue
@@ -1,87 +1,113 @@
 <!-- 查看成员 -->
 <template lang="html">
-  <div class="box">
-    <div>
-      <div class="search-box">
-        <input v-model="organizationId" class="input search-input" type="text" placeholder="请输入你要查看的成员">
-        <button @click="searchMember()" class="button" type="button" name="button">查找成员</button>
-      </div>
-        <button @click="addMember()" class="button add-role-button" type="button" name="button">添加成员</button>
-        <button @click="synchronizeMember()" class="button add-role-button" type="button" name="button">同步成员</button>
-    </div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>成员名</th>
-          <th>创建者ID</th>
-          <th>描述</th>
-          <th>最大容量</th>
-          <th>当前容量</th>
-          <th>创建时间</th>
-          <th>更新时间</th>
-          <th>操作</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in MemberData">
-          <td>{{ item.id }}</td>
-          <td>{{ item.name }}</td>
-          <td>{{ item.creator_id }}</td>
-          <td> {{ item.description }}</td>
-          <td>{{ item.max }}</td>
-          <td>{{ item.current }}</td>
-          <td>{{ item.created_at }}</td>
-          <td>{{ item.updated_at }}</td>
-          <td>
-            <button @click="deleteMember(index)" class="button" type="button" name="button">删除成员</button>
-            <button @click="synchronizeMember(index)"  class="button" type="button" name="button">编辑成员</button>
-            <button  class="button" type="button" name="button">查看成员</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="modal" v-bind:class="{'is-active': isShowModal}">
+    <div class="modal-background"></div>
+    <div class="modal-content member-content">
+      <div class="box member-box">
+        <div>
+          <div class="search-box">
+            <input v-model="searchKey" class="input search-input" type="text" placeholder="请输入你要查看的成员">
+            <button @click="searchMember()" class="button" type="button" name="button">查找成员</button>
+          </div>
+            <button @click="addMember()" class="button add-role-button" type="button" name="button">添加成员</button>
+            <button @click="synchronizeMember()" class="button add-role-button" type="button" name="button">同步成员</button>
+        </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <!-- <th>ID</th> -->
+              <th>成员名</th>
+              <!-- <th>创建者ID</th> -->
+              <th>描述</th>
+              <th>最大容量</th>
+              <th>当前容量</th>
+              <th>创建时间</th>
+              <th>更新时间</th>
+              <th>操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item,index) in memberData">
+              <!-- <td>{{ item.id }}</td> -->
+              <td>{{ item.name }}</td>
+              <!-- <td>{{ item.creator_id }}</td> -->
+              <td>{{ item.description }}</td>
+              <td>{{ item.max }}</td>
+              <td>{{ item.current }}</td>
+              <td>{{ item.created_at }}</td>
+              <td>{{ item.updated_at }}</td>
+              <td>
+                <button @click="deleteMember(index)" class="button" type="button" name="button">删除成员</button>
+                <!-- <button @click="synchronizeMember(index)"  class="button" type="button" name="button">编辑成员</button> -->
+                <!-- <button  class="button" type="button" name="button">查看成员</button> -->
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-    <add-member ref="addMember"></add-member>
-    <synchronize-member ref="synchronizeMember"  :edit-data="editData"></synchronize-member>
+        <pagination v-bind:pagination-data="paginationData"
+                    v-model="data"
+        ></pagination>
+
+        <add-member ref="addMember"
+                    v-bind:organization-id="organizationId"
+                    v-on:getMember="getMember"
+        ></add-member>
+
+        <synchronize-member ref="synchronizeMember"
+                            v-bind:organization-id="organizationId"
+                            v-on:getMember="getMember"
+        ></synchronize-member>
+      </div>
+    </div>
+    <button @click="switchModal()" class="modal-close is-large" aria-label="close"></button>
   </div>
 </template>
 
 <script>
-import AddMember from './AddMember'
+import Pagination from './../Pagination'
 import SynchronizeMember from './SynchronizeMember'
+import AddMember from './AddMember'
 
 export default {
   data() {
     return {
       token: null,
-      "MemberData": [
-          {
-              "id": 1,
-              "name": "admin",
-              "email": "admin@email.com",
-              "number": null,
-              "phone": null,
-              "qq": null,
-              "created_at": "2018-01-13 07:59:11",
-              "updated_at": "2018-01-13 07:59:11",
-              "pivot": {
-                  "user_id": "2",
-                  "organization_id": "1"
-              }
-          }
-      ],
       isShowModal: false,
-      // MemberData: null,
-      organizationId: null,
+      memberData: null,
+      searchKey: null,
       editData: null,  // 当前编辑的成员数据
+      paginationData: null,
+      data: null,
+      organizationId: null,
     }
   },
   components: {
-    AddMember,
+    Pagination,
     SynchronizeMember,
+    AddMember,
   },
+  props: [
+    'currentOrganizationData',
+  ],
   methods: {
+    switchModal: function () {
+      const that = this;
+      that.isShowModal = !that.isShowModal;
+    },
+    clearWords: function () {
+      const that = this;
+      that.memberData.id = '';
+      that.memberData.name = '';
+      that.memberData.email = '';
+      that.memberData.number = '';
+      that.memberData.phone = '';
+      that.memberData.qq = '';
+      that.memberData.created_at = '';
+      that.memberData.updated_at = '';
+      that.memberData.pivot.user_id = '';
+      that.memberData.pivot.organization_id = '';
+    },
     addMember: function() {
       const that = this;
       that.$refs.addMember.switchModal();
@@ -92,16 +118,17 @@ export default {
     },
     getMember: function () {
       const that = this;
+      let id = that.currentOrganizationData['id'];
       axios({
         method: 'get',
-        url: `${this.GLOBAL.localDomain}/api/v1/organizations/2/users`,
+        url: `${this.GLOBAL.localDomain}/api/v1/organizations/${id}/users`,
         headers: {
           'Accept': 'application/json',
           'Authorization': that.token
         }
       }).then(res => {
         that.memberData = res.data.data;
-         
+        that.paginationData = res.data.links;
       }).catch(err => {
         console.log(err)
       })
@@ -110,7 +137,7 @@ export default {
       const that = this;
       axios({
         method: 'get',
-        url: `${this.GLOBAL.localDomain}/api/v1/organizations/${that.organizationId}`,
+        url: `${this.GLOBAL.localDomain}/api/v1/organizations/${that.searchKey}`,
         headers: {
           'Accept': 'application/json',
           'Authorization': that.token
@@ -122,7 +149,6 @@ export default {
         console.log(err)
       })
     },
-    // 删除成员 ??删除需要的参数
     deleteMember: function (index) {
       const that = this;
       let id = that.memberData[index]['id'];
@@ -136,7 +162,9 @@ export default {
             'Authorization': that.token
           }
         }).then(res => {
+          alert('删除成功！')
         }).catch(err => {
+          alert('删除失败')
           console.log(err)
         })
       }
@@ -144,9 +172,18 @@ export default {
   },
   created() {
     this.token = sessionStorage.getItem('token');
-    // this.getMember();
   },
   watch: {
+    data:function (value, oldValue) {
+      const that = this;
+      that.organizationData = value.data;
+      that.paginationData = value.links;
+    },
+    currentOrganizationData: function (value, oldValue) {
+      const that = this;
+      that.organizationId = value.id;
+      this.getMember();
+    }
   }
 }
 </script>
@@ -168,15 +205,10 @@ table {
 .add-role-button {
   margin-left: 20px;
 }
-.box-item {
-  margin-bottom: 20px;
-  input  {
-    display: inline-block;
-    width: 300px;
-  }
-  label {
-    display: inline-block;
-    width: 130px;
-  }
+.member-box {
+  width: 1300px;
+}
+.member-content {
+  width: 1300px;
 }
 </style>
--- resources/assets/js/components/Member/synchronizeMember.vue
+++ resources/assets/js/components/Member/synchronizeMember.vue
@@ -3,23 +3,17 @@
     <div class="modal-background"></div>
     <div class="modal-card">
       <header class="modal-card-head">
-        <p class="modal-card-title">添加成员</p>
+        <p class="modal-card-title">同步成员</p>
         <button @click="switchModal()" class="delete" aria-label="close"></button>
       </header>
       <section class="modal-card-body">
         <div class="box-item">
-          <!-- // ?? 添加成员参数 -->
           <label>users</label>
-          <input v-model="memberData.name" class="input" type="text" placeholder="请输入成员名">
-        </div>
-
-        <div class="box-item">
-          <label>users</label>
-          <input v-model="memberData.max" class="input" type="number">
+          <input v-model="memberData.users" class="input" type="number">
         </div>
       </section>
       <footer class="modal-card-foot">
-        <button @click="synchronizeMember()" class="button is-success">确认</button>
+        <button @click="synchronousMember()" class="button is-success">确认</button>
         <button  @click="switchModal()" class="button">取消</button>
       </footer>
     </div>
@@ -34,12 +28,14 @@ export default {
       isShowModal: false,
       memberData: {
         users: null,
-        users: null,
       },
       token: null,
       permissionId: null,
     }
   },
+  props: [
+    'organizationId',
+  ],
   components: {
   },
   methods: {
@@ -47,26 +43,33 @@ export default {
       const that = this;
       that.isShowModal = !that.isShowModal;
     },
-    // 添加成员
-    synchronizeMember: function () {
+    clearWords: function () {
       const that = this;
+      that.memberData.users = '';
+    },
+    synchronousMember: function () {
+      const that = this;
+      let id = that.organizationId;
       axios({
         method: 'post',
-        url: `${this.GLOBAL.localDomain}/api/v1/organizations`,
+        url: `${this.GLOBAL.localDomain}/api/v1/organizations/${id}/users`,
         headers: {
           'Accept': 'application/json',
-          'Authorization': that.token
+          'Authorization': that.token,
         },
-        body: {
-        //   users: that.memberData.
-        //   users: that.memberData.
+        params: {
+          users: null,
         }
       }).then(res => {
-         
+        alert('同步成功');
+        that.$emit('getMember');   //第一个参数名为调用的方法名，第二个参数为需要传递的参数
+        that.switchModal();
       }).catch(err => {
-        console.log(err)
+        alert('同步失败');
+        console.log(err);
+        that.clearWords();
       })
-    },
+    }
   },
   created() {
     this.token = sessionStorage.getItem('token');
