b33127029d23648071a88311f6a8497c1b3426d1
Yfling <598184085@qq.com>
2018-03-22 10:30:21 +0800

    teaching component improvement

--- resources/assets/js/components/Teaching/AddTeaching.vue
+++ resources/assets/js/components/Teaching/AddTeaching.vue
@@ -9,12 +9,23 @@
       <section class="modal-card-body">
         <div class="box-item">
           <label>授课名称</label>
-          <input class="input" type="text" placeholder="请输入授课名">
+          <input v-model="teachingData.name" class="input" type="text">
         </div>
-
         <div class="box-item">
-          <label>授课名称</label>
-          <input class="input" type="text">
+          <label>课程ID</label>
+          <input v-model="teachingData.course_id" class="input" type="text">
+        </div>
+        <div class="box-item">
+          <label>最大值</label>
+          <input v-model="teachingData.max" class="input" type="text">
+        </div>
+        <div class="box-item">
+          <label>允许组织的ID集合</label>
+          <input v-model="teachingData.allowable_organization_ids" class="input" type="text" placeholder="若多个id请用逗号分隔">
+        </div>
+        <div class="box-item">
+          <label>描述</label>
+          <input v-model="teachingData.describe" class="input" type="text">
         </div>
       </section>
       <footer class="modal-card-foot">
@@ -30,10 +41,14 @@
 export default {
   data() {
     return {
+      token: null,
       isShowModal: false,
       teachingData: {
-        to: null,
-        data: null,
+        name: null,
+        course_id: null,
+        max: null,
+        allowable_organization_ids: null,
+        describe: null,
       }
     }
   },
@@ -44,24 +59,45 @@ export default {
       const that = this;
       that.isShowModal = !that.isShowModal;
     },
+    clearWords: function () {
+      const that = this;
+      that.teachingData.name = '';
+      that.teachingData.course_id = '';
+      that.teachingData.max = '';
+      that.teachingData.allowable_organization_ids = '';
+      that.teachingData.describe = '';
+    },
     addTeaching: function () {
       const that = this;
+      let ids = [];
       axios({
         method: 'post',
-        url: `${this.GLOBAL.localDomain}/api/v1/teachings/`,
+        url: `${this.GLOBAL.localDomain}/api/v1/lectures`,
         headers: {
           'Accept': 'application/json',
           'Authorization': that.token
         },
-        body: {
-          to: that.teachingData.to,
-          data: that.teachingData.data
+        // 优化：检测id是否合法，ids是否是数组
+        params: {
+          name: that.teachingData.name,
+          course_id: that.teachingData.course_id,
+          max: that.teachingData.max,
+          allowable_organization_ids: that.allowable_organization_ids,  // ?? params not allow send array
+          describe: that.teachingData.describe
         }
       }).then(res => {
+        alert('添加成功');
+        that.$emit('getTeaching');   //第一个参数名为调用的方法名，第二个参数为需要传递的参数
+        that.switchModal();
       }).catch(err => {
-        console.log(err)
+        alert('添加失败');
+        console.log(err);
+        that.clearWords();
       })
     }
+  },
+  created() {
+    this.token = sessionStorage.getItem('token');
   }
 }
 </script>
