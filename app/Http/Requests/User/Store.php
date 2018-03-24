3d0809fe3707afbe7eef27d3fb12eb59fd818da9
badtudou <badtudou@gmail.com>
2018-03-24 12:09:00 +0800

    数据验证增加中文提示

--- app/Http/Requests/User/Store.php
+++ app/Http/Requests/User/Store.php
@@ -31,4 +31,18 @@ class Store extends FormRequest
             'c_password' => 'required|same:password',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '用户名不能为空',
+            'email.required'  => '注册邮箱不能为空',
+            'email.email'  => '注册邮箱格式有误',
+            'email.unique'  => '该邮箱已被注册',
+            'captcha.required'  => '验证码不能为空',
+            'password.required'  => '密码不能为空',
+            'c_password.required' => '两次输入的密码不一致',
+            'c_password.same' => '两次输入的密码不一致',
+        ];
+}
 }
