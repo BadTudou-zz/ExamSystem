fe9e8a9539b833e83d2f81fcf62be10b4e1974f0
badtudou <badtudou@gmail.com>
2018-03-25 11:38:52 +0800

    自定义分页时，返回完整url

--- app/Util/OrmUtil.php
+++ app/Util/OrmUtil.php
@@ -10,6 +10,7 @@ class OrmUtil
     public static function paginate($items, $perPage = 15, $page = null, $options = [])
     {
         $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $options = $options ? : ['path' => Paginator::resolveCurrentPath()];
         $items = $items instanceof Collection ? $items : Collection::make($items);
         return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
     }
