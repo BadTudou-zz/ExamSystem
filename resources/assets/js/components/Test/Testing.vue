c439b97e98975cf0803a6280d1a247fc5fdf1dfd
Yfling <598184085@qq.com>
2018-03-31 11:30:54 +0800

    add: complete the student examination logic

--- resources/assets/js/components/Test/Testing.vue
+++ resources/assets/js/components/Test/Testing.vue
@@ -2,26 +2,26 @@
 <template lang="html">
   <div class="box">
 
-    <button class="button" type="button" name="button">退出考试</button>
-    <button class="button is-primary" type="button" name="button">完成考试</button>
-
-    <div  v-for="(item,index) in questionData" class="message box">
-      <div class="notification">
-        <p class="question">        考试id：{{ item.id }}
-          &nbsp;&nbsp;&nbsp;&nbsp; 考试题目：{{ item.title }}
-          &nbsp;&nbsp;&nbsp;&nbsp; 考试类型：{{ item.question_type }}
-          &nbsp;&nbsp;&nbsp;&nbsp; 难易程度：{{ item.level_type }}
-        </p>
-        {{ item.body }}
-        <p class="time">{{item.created_at}}</p>
-      </div>
-      <div>
-        <p>回复：{{ item.answer_comment }}</p>
-      </div>
+    <div v-if="isLoading">
+      <img class="loading" src="../../../img/loading.gif" alt="">
     </div>
 
-    <div v-show="isWaiting">
-      <img src="" alt="">
+    <div v-else>
+      <button @click="stopTest()" class="button is-info finish-test" type="button" name="button">结束考试</button>
+      <div  v-for="(item,index) in questionData" class="message box">
+        <div class="notification">
+          <p class="question">        考试id：{{ item.id }}
+            &nbsp;&nbsp;&nbsp;&nbsp; 考试题目：{{ item.title }}
+            &nbsp;&nbsp;&nbsp;&nbsp; 考试类型：{{ item.question_type }}
+            &nbsp;&nbsp;&nbsp;&nbsp; 难易程度：{{ item.level_type }}
+          </p>
+          问题：{{ item.body }}
+          <p class="time">{{item.created_at}}</p>
+        </div>
+        <div>
+          <p>回复：{{ item.answer_comment }}</p>
+        </div>
+      </div>
     </div>
 
   </div>
@@ -38,12 +38,14 @@ export default {
        chapterIds: [],
        questionIds: [],
        temporaryQuestionIds: [],
+       isLoading: true,
     }
   },
   components: {
   },
   props: [
-    'paperId'
+    'paperId',
+    'examId',
   ],
   methods: {
     searchExaminationPaper: function (paperId) {
@@ -72,6 +74,7 @@ export default {
     searchChapter: function (chapterId) {
       const that = this;
       let paperId = that.paperId;
+      let questionIdsArray = [];
       axios({
         method: 'get',
         url: `${this.GLOBAL.localDomain}/api/v1/papers/${paperId}/sections/${chapterId}`,
@@ -80,14 +83,17 @@ export default {
           'Authorization': that.token
         }
       }).then(res => {
-        let questionIds = res.data.data.questions;
-        let questionIdsArray = questionIds.split(',')
-        that.questionIds = that.questionIds.concat(questionIdsArray);
 
-        for (let i = 0; i < questionIds.length; i++) {
-          let questionId = questionIds[i];
+        let questionIds = res.data.data.questions;
+        questionIdsArray = questionIds.split(',')
+
+        for (let i = 0; i < questionIdsArray.length; i++) {
+          // if (questionIds[i] === ',') return;
+          let questionId = questionIdsArray[i];
           that.searchQuestion(questionId);
         }
+
+        that.isLoading = false;
 
       }).catch(err => {
         console.log(err)
@@ -115,7 +121,35 @@ export default {
     uniqArray: function (arr) {
       let uniq = [...new Set(arr)];
       return uniq;
-    }
+    },
+    quitTest: function () {
+      const that = this;
+
+    },
+    stopTest: function () {
+      const that = this;
+      let id = that.examId;
+      alert('已结束');
+      // axios({
+      //   method: 'post',
+      //   url: `${this.GLOBAL.localDomain}/api/v1/exams/${id}/stop`,
+      //   headers: {
+      //     'Accept': 'application/json',
+      //     'Authorization': that.token
+      //   }
+      // }).then(res => {
+      //   alert('已结束');
+      // }).catch(err => {
+      //   let errMsg = err.response.data.error;
+      //   if (errMsg) {
+      //     alert(errMsg);
+      //   }
+      //   else {
+      //     alert('结束失败，请稍后再试');
+      //   }
+      //   console.log(err)
+      // })
+    },
   },
   computed: {
   },
@@ -134,17 +168,18 @@ export default {
         that.searchChapter(chapterId);
       }
     },
-    // questionIds: function (value, oldValue) {
-    //   const that = this;
-    //   for (let i = 0; i < value.length; i++) {
-    //     let questionId = value[i];
-    //     that.searchQuestion(questionId);
-    //   }
-    // },
+    questionIds: function (value, oldValue) {
+      const that = this;
+      // console.log(value)
+      // for (let i = 0; i < value.length; i++) {
+      //   let questionId = value[i];
+      //   that.searchQuestion(questionId);
+      // }
+    },
     // questionData: function (value, oldValue) {
     //   const that = this;
     //   console.log('questionData数据变更');
-    // }
+    // },
 
   }
 }
@@ -168,5 +203,14 @@ export default {
 .question {
   text-align: left;
   margin-bottom: 10px;
+}
+.loading {
+  width: 50px;
+  height: 50px;
+  margin: 0 auto;
+  display: block;
+}
+.finish-test {
+  margin-left: 20px;
 }
 </style>
