ac05dd63a18e0a5c5c9be61ef5a94b2e7c2c490f
badtudou <badtudou@gmail.com>
2018-04-19 09:52:12 +0800

    修改用户全部权限接口

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -11,7 +11,7 @@ use App\Http\Resources\UserResource;
 use App\Http\Resources\OrganizationCollection;
 use App\Http\Resources\LectureCollection;
 use App\Http\Resources\RoleCollection;
-use App\Http\Resources\PermissionCollection;
+use App\Http\Resources\PermissionResource;
 use App\Http\Resources\ApplicationResource;
 use App\Http\Resources\ExamResource;
 use App\Http\Resources\ApplicationCollection;
@@ -94,7 +94,7 @@ class UserController extends Controller
         $permissions = User::find($id)->roles()->get()->map(function ($role){
             return $role->perms()->get();
         });
-        return new PermissionCollection(OrmUtil::paginate($permissions));
+        return PermissionResource::collection(OrmUtil::paginate(count($permissions)?$permissions[0]:null));
     }
 
     public function applications(ShowUser $request)
