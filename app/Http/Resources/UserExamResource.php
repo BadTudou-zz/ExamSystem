27784ff5c601159952a9817b73fe6c047872d13b
badtudou <badtudou@gmail.com>
2018-04-27 14:14:37 +0800

    添加接口：查看考试的全部分数

---
+++ app/Http/Resources/UserExamResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class UserExamResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'exam_id' => $this->exam_id,
+            'answers' => $this->pivot->answers,
+            'score' => $this->pivot->score,
+            'begin_at' => $this->pivot->begin_at,
+            'correct_at' => $this->pivot->correct_at,
+            'finish_at' => $this->pivot->finish_at
+        ];
+    }
+}
