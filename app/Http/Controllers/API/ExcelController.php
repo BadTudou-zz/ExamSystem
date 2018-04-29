69d462c2f6621b2353f22f9c01a54dc583f1fd5d
StubbornGrass <g1090035743@gmail.com>
2018-04-29 20:14:49 +0800

    导入excel到题目完成

--- app/Http/Controllers/API/ExcelController.php
+++ app/Http/Controllers/API/ExcelController.php
@@ -2,32 +2,65 @@
 
 namespace App\Http\Controllers\API;
 
+use App\QuestionType;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Exam\Index as IndexExam;
-use App\Http\Requests\Exam\Store as StoreExam;
-use App\Http\Requests\Exam\Update as UpdateExam;
-use App\Http\Requests\Exam\Show as ShowExam;
-use App\Http\Requests\Exam\Destroy as DestroyExam;
-use App\Http\Requests\Exam\Answer as AnswerExam;
-use App\Http\Requests\Exam\Begin as BeginExam;
-use App\Http\Requests\Exam\AddUsers as AddUsersToExam;
-use App\Http\Requests\Exam\DeleteUsers as DeleteUsersFromExam;
-use App\Http\Resources\ExamCollection;
-use App\Http\Resources\ExamResource;
-use App\Http\Resources\UserCollection;
-use App\Http\Resources\UserExamResource;
-use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
-use App\Jobs\CorrecExam;
-use App\Util\OrmUtil;
+
 
 use App\Exam;
 use App\User;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ExcelController extends Controller
 {
-        public function test () {
-            dd(public_path());
+        public function import () {
+
+            $filePath = $this->upload($_FILES);
+            Excel::load($filePath, function($reader) {
+                $allType = QuestionType::get(["name","id"])->toArray();
+                $newType = [];
+                foreach ($allType as $value){
+                    $newType[$value["id"]] = $value['name'];
+                }
+                $reader = $reader->getSheet(0);
+                //获取表中的数据
+                $data = $reader->toArray();
+                $questions = [];
+                for ($i = 1;$i<count($data);$i ++){
+                    $questions[$i]['type_id'] = array_search($data[$i][0], $newType);
+                    $questions[$i]['level_type'] = $data[$i][1];
+                    $questions[$i]['title'] = $data[$i][2];
+                    $questions[$i]['body'] = $data[$i][3];
+                    $questions[$i]['answer']  = $data[$i][4];
+                    $questions[$i]['answer_comment']  = $data[$i][5];
+                    $questions[$i]['created_at'] = (date('Y-m-d H:i:s', time()));
+                    $questions[$i]['updated_at'] = (date('Y-m-d H:i:s', time()));
+                }
+                DB::table("questions")->insert(
+                    $questions
+                );
+
+            });
+        }
+
+        protected  function upload ($files){
+            // 把当前上传文档的时间精确到秒作为文件名重新赋值给上传文件作为它的新的文件名
+            $date = date('Y-m-dHis',time());
+            // 以.来截取文件的后缀名
+            $uptype = explode(".", $files["document"]["name"]);
+            // 然后把当前时间加上后缀名就是该文档的新名称。
+            $album_picture_name = $date.".".$uptype[1];
+            // 给上传的文档重新命名
+            $files["document"]["name"] = $album_picture_name;
+            //定义上传文件存储位置
+            $path =  public_path() . DIRECTORY_SEPARATOR . 'questions' . DIRECTORY_SEPARATOR .  $files["document"]["name"];
+
+            // 移动文件到自己建的文件夹下
+            if(move_uploaded_file($files["document"]["tmp_name"], $path)){
+
+                return $path;
+            }
+            return false;
         }
 }
