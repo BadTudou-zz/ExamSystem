ef0f1f09bebaa81b93f75618ffac8b775455d92f
Yfling <598184085@qq.com>
2018-04-30 17:48:55 +0800

    fix: register to login.

--- resources/assets/js/components/Video/AddVideo.vue
+++ resources/assets/js/components/Video/AddVideo.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="modal" :class="{'is-active' : isShowModal}">
+  <!-- <div class="modal" :class="{'is-active' : isShowModal}">
     <div class="modal-background"></div>
     <div class="modal-card">
       <header class="modal-card-head">
@@ -21,64 +21,64 @@
         <button  @click="switchModal()" class="button">取消</button>
       </footer>
     </div>
+  </div> -->
+  <div class="">
+    ss
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      isShowModal: false,
-      videoData: {
-        videoName: '',
-        kp: '',
-      },
-    }
-  },
-  components: {
-  },
-  methods: {
-    switchModal: function () {
-      const that = this;
-      that.isShowModal = !that.isShowModal;
-    },
-    clearWords: function () {
-      const that = this;
-      that.videoData.name = '';
-      that.videoData.display_name = '';
-      that.videoData.descripe = '';
-      that.videoData.number = '';
-    },
-    addVideo: function () {
-      const that = this;
-      axios({
-        method: 'post',
-        url: `${this.GLOBAL.localDomain}/api/v1/courses`,
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': sessionStorage.getItem('token'),
-        },
-        params: {
-          name: that.videoData.name,
-          display_name: that.videoData.display_name,
-          descripe: that.videoData.descripe,
-          number: that.videoData.number
-        }
-      }).then(res => {
-        alert('添加成功');
-        that.$emit('getVideo');   //第一个参数名为调用的方法名，第二个参数为需要传递的参数
-        that.switchModal();
-      }).catch(err => {
-        alert('添加失败');
-        console.log(err);
-        that.clearWords();
-      })
-    }
-  },
-  created() {
-
-  }
-}
+// export default {
+//   data() {
+//     return {
+//       isShowModal: false,
+//       videoData: {
+//         videoName: '',
+//         kp: '',
+//       },
+//     }
+//   },
+//   components: {
+//   },
+//   methods: {
+//     switchModal: function () {
+//       const that = this;
+//       that.isShowModal = !that.isShowModal;
+//     },
+//     clearWords: function () {
+//       const that = this;
+//       that.videoData.name = '';
+//       that.videoData.display_name = '';
+//       that.videoData.descripe = '';
+//       that.videoData.number = '';
+//     },
+//     addVideo: function () {
+//       const that = this;
+//       axios({
+//         method: 'post',
+//         url: `${this.GLOBAL.localDomain}/api/v1/courses`,
+//         headers: {
+//           'Accept': 'application/json',
+//           'Authorization': sessionStorage.getItem('token'),
+//         },
+//         params: {
+//           videoName: that.videoData.videoName,
+//           kp: that.videoData.kp,
+//         }
+//       }).then(res => {
+//         alert('添加成功');
+//         that.$emit('getVideo');   //第一个参数名为调用的方法名，第二个参数为需要传递的参数
+//         that.switchModal();
+//       }).catch(err => {
+//         alert('添加失败');
+//         console.log(err);
+//         that.clearWords();
+//       })
+//     }
+//   },
+//   created() {
+//   }
+// }
 </script>
 
 <style>
