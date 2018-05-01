db45e0de1f90695ec3f654ffb8c5d67477f8b42b
badtudou <badtudou@gmail.com>
2018-05-01 12:07:49 +0800

    WebSocket添加订阅

--- app/Http/Sockets/WebSocket.php
+++ app/Http/Sockets/WebSocket.php
@@ -14,8 +14,11 @@ class WebSocket extends BaseSocketListener
      *
      * @var \SplObjectStorage
      */
-    protected $clients;
-    const AUTHENTICATION = 'authentication';
+    protected $clients = [];
+    const AUTHENTICATION = 'authentication'; //认证
+    const SUBSCRIBE = 'subscribe'; //订阅
+    const UNSUBSCRIBE = 'unsubscribe'; //退订
+    const PUBLISH = 'publish'; //发布
     /**
      * Socket constructor.
      */
@@ -29,7 +32,6 @@ class WebSocket extends BaseSocketListener
      */
     public function onOpen(ConnectionInterface $conn)
     {
-        $this->clients->attach($conn);
     }
 
     /**
@@ -42,7 +44,12 @@ class WebSocket extends BaseSocketListener
             $data = json_decode($msg);
 
             if (! is_object($data)) {
-                return $this->sendData($from, 'server', '参数格式错误', 500);
+                return $this->sendData($from, 'server', '参数格式错误', null, 422);
+            }
+
+            // 身份验证
+            if (! $this->auth($from, $data)){
+                return $this->sendData($from, self::AUTHENTICATION, '认证失败', null, 401);
             }
 
             // // 签名验证
@@ -55,11 +62,9 @@ class WebSocket extends BaseSocketListener
                     $this->authentication($from, $data);
                     break;
                
-                // case 'subscribe':
-                //     $client = $this->getClientByContent($from, $msg);
-                //     if (! $client) {
-                //         return ;
-                //     }
+                case self::SUBSCRIBE:
+                    $this->subscribe($from, $data);
+                    break;
 
                 //     $data =  json_decode($msg);
                 //     if ($data->scene == 'paid_result') {
@@ -76,7 +81,7 @@ class WebSocket extends BaseSocketListener
                 //     break;
 
                 default:
-                    $this->sendData($from, 'server', '不支持的请求', null, 500);
+                    $this->sendData($from, 'server', '不支持的请求', null, 400);
                     break;
             }
 
@@ -85,7 +90,6 @@ class WebSocket extends BaseSocketListener
             logger($e);
         }
 
-           
             
     }
 
@@ -120,13 +124,99 @@ class WebSocket extends BaseSocketListener
         ));
     }
 
+    // 解析客户端
+    protected function auth($client, $params)
+    {
+        return isset($params->token) && $this->parseToken($params->token);
+    }
+
     // 身份验证
     protected function authentication($client, $params)
     {
-        if (isset($params->data->token) && ($user = $this->parseToken($params->data->token)) ) {
+        if (isset($params->token) && ($user = $this->parseToken($params->token)) ) {
+            $this->addClient($user, $client);
             $this->sendData($client, self::AUTHENTICATION, '认证成功', $user, 200);
         } else {
             $this->sendData($client, self::AUTHENTICATION, '认证失败', $user, 401);
         }
+    }
+
+    // 验证客户端
+    protected function verifyClient($conn)
+    {
+        $client = $this->getClientByConn($conn);
+        if (! $client) {
+            return $this->sendData($conn, 'client', '非法客户端', null, 400);
+        }
+
+        return $client;
+    
+    }
+
+    // 订阅
+    protected function subscribe($conn, $params)
+    {
+        $client = $this->verifyClient($conn);
+        if (isset($params->data->channel)) {
+            if (! in_array($params->data->channel, $client->channel)) {
+                array_push($client->channel, $params->data->channel);
+            }
+            return $this->sendData($conn, self::SUBSCRIBE, '订阅频道成功', $client->channel, 200);
+        } else {
+            return $this->sendData($conn, self::SUBSCRIBE, '订阅频道失败', $client->channel, 400);
+        }
+    }
+
+    // 退订
+    protected function unsubscribe()
+    {
+
+    }
+
+    // 发布
+    protected function publish()
+    {
+
+    }
+
+     // 添加客户端
+    public function addClient($user, $conn)
+    {
+        $client = (object)['user' => $user, 'client' => (object)$conn, 'channel' => []];
+
+        if ($oldClient = $this->getClientByConn($conn)) {
+            $this->deleteClient($oldClient);
+        }
+
+        $this->clients->attach($client);
+        return $client;
+    }
+
+    // 根据连接获取客户端
+    protected function getClientByConn($conn)
+    {
+        foreach ($this->clients as $client) {
+            if ($client->client == $conn) {
+                return $client;
+            }
+        }
+        return null;
+    }
+
+    // 删除客户端
+    protected function deleteClient($client)
+    {
+        $this->clients->detach($client);
+    }
+
+    // 根据用户获取客户端
+    protected function getClientByUser($user)
+    {
+        foreach ($this->clients as $client) {
+            if ($client->user == $user) {
+                return $client;
+            }
+        }
+        return null;
     }
 }
