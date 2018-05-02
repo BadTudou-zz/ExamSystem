fc0e18e6cb6e2ef2bad6fdf09eb27a5061b4e18b
Yfling <598184085@qq.com>
2018-05-02 17:09:51 +0800

    add: empty.vue

---
+++ resources/assets/js/components/Empty.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+  },
+  methods: {
+
+  },
+  created() {
+  },
+  watch: {
+  }
+}
+</script>
+
+<style >
+</style>
