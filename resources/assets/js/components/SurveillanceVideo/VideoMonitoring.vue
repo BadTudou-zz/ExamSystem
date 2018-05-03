09c3a8c8f94dd04e49616ad4140544412e3f1add
Yfling <598184085@qq.com>
2018-05-03 12:24:50 +0800

    add videoMonitoring.vue

---
+++ resources/assets/js/components/SurveillanceVideo/VideoMonitoring.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <div class="box">
+    <div class="columns columns-box">
+      <div class="box column">1</div>
+      <div class="box column">2</div>
+      <div class="box column">3</div>
+      <div class="box column">4</div>
+    </div>
+    <div class="columns columns-box">
+      <div class="box column">1</div>
+      <div class="box column">2</div>
+      <div class="box column">3</div>
+      <div class="box column">4</div>
+    </div>
+    <div class="columns columns-box">
+      <div class="box column">1</div>
+      <div class="box column">2</div>
+      <div class="box column">3</div>
+      <div class="box column">4</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+  },
+  methods: {
+
+  },
+  created() {
+  },
+  watch: {
+  }
+}
+</script>
+
+<style scoped>
+.columns-box .box {
+  padding: 0px;
+  height: 100px;
+}
+</style>
--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,3 +1,4 @@
+
 export default[
   {path: '/', redirect: '/personalData'},
   // 主页
@@ -74,6 +75,8 @@ export default[
   { path: '/Setting', component: require('./components/Setting.vue') },
   // webrtc
   // { path: '/webrtc', component: require('./components/SurveillanceVideo/webrtc.vue') },
+  // webrtc
+  { path: '/VideoMonitoring', component: require('./components/SurveillanceVideo/VideoMonitoring.vue') },
   // 管理中心
   // { path: '/Setting', component: require('./components/SurveillanceVideo/Setting.vue') },
 ]
