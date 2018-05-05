b9456c118ac80bb716567285b0c024040fd038ba
StubbornGrass <g1090035743@gmail.com>
2018-05-05 18:05:52 +0800

    域名同步

--- resources/assets/js/components/VideoInfo/AddVideoInfo.vue
+++ resources/assets/js/components/VideoInfo/AddVideoInfo.vue
@@ -95,7 +95,7 @@ export default {
 
       axios({
         method: 'POST',
-        url: `http://www.badtudou.com/api/v1/upload/lecture/insert/video`,
+        url: `${this.GLOBAL.localDomain}/api/v1/upload/lecture/insert/video`,
         headers: {
           'Accept': 'application/json',
           'Authorization': sessionStorage.getItem('token'),
