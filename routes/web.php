92a2b3d9f4a3f40d67b301a6ab3f339185e3e102
badtudou <badtudou@gmail.com>
2018-05-05 21:42:33 +0800

    修改接口

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('index');
+
+Route::get('/home', 'HomeController@home')->name('home');
 
 
