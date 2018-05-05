7bf34d61bf6e9b0ffbb61a66b9b4e0b045a20a54
badtudou <badtudou@gmail.com>
2018-05-06 01:08:58 +0800

    修改接口：答案解析

---
+++ app/Http/Resources/ExamCheckResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ExamCheckResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'user_id' => $this->pivot->user_id,
+            'exam_id' => $this->pivot->exam_id,
+            'begin_at' => $this->pivot->begin_at,
+            'finish_at' => $this->pivot->finish_at,
+            'correct_at' => $this->pivot->correct_at,
+            'score' => $this->pivot->score,
+            'answers' => json_decode($this->pivot->answers),
+            'results' => json_decode($this->pivot->results)
+        ];
+    }
+}
