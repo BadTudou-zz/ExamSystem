53b8b6fefda199049ba2260f84fd596dc6987994
Yfling <598184085@qq.com>
2018-05-06 23:16:37 +0800

    delete navigation routes

--- resources/assets/js/routes2.js
+++ resources/assets/js/routes2.js
@@ -36,8 +36,8 @@ import TestingTest from './components/PersonalCenter/TestingTest.vue'
 
 import Setting from './components/Setting.vue'
 // import VideoMonitoring from './components/SurveillanceVideo/VideoMonitoring.vue'
-import webrtc from './components/SurveillanceVideo/webrtc.vue'
-import webrtcServer from './components/SurveillanceVideo/webrtcServer.vue'
+// import webrtc from './components/SurveillanceVideo/webrtc.vue'
+// import webrtcServer from './components/SurveillanceVideo/webrtcServer.vue'
 import Practice from './components/Practice/Practice.vue'
 import Upload from './components/Upload/Upload.vue'
 // import Main from './components/App.vue'
@@ -209,20 +209,20 @@ export default [
       requireAuth: true
     }
   },
-  {
-    path: '/webrtc',
-    component: webrtc,
-    meta: {
-      requireAuth: true
-    }
-  },
-  {
-    path: '/webrtcServer',
-    component: webrtcServer,
-    meta: {
-      requireAuth: true
-    }
-  },
+  // {
+  //   path: '/webrtc',
+  //   component: webrtc,
+  //   meta: {
+  //     requireAuth: true
+  //   }
+  // },
+  // {
+  //   path: '/webrtcServer',
+  //   component: webrtcServer,
+  //   meta: {
+  //     requireAuth: true
+  //   }
+  // },
   {
     path: '/PersonalData',
     component: PersonalData,
