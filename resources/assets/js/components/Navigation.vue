5b28b22b3387e0dd79f230000ea2852a104281d6
Yfling <598184085@qq.com>
2018-05-07 22:25:57 +0800

    fix

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -38,7 +38,7 @@
                   <template slot="title"></template>
                   <!-- <el-menu-item index="7-1"><router-link to="/roleData">角色</router-link></el-menu-item> -->
                   <el-menu-item index="7-2"><router-link to="/permissionData">权限</router-link></el-menu-item>
-                  <el-menu-item index="7-3"><router-link to="/organizationData">组织</router-link></el-menu-item>
+                  <el-menu-item index="7-3"><router-link to="/organization">组织</router-link></el-menu-item>
                 </el-menu-item-group>
               </el-submenu>
 
